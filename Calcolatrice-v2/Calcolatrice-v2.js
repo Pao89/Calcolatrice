400c528b6e63917348ac2fa804b5298da3756eda
Pao89 <paolonicolo95@gmail.com>
2017-06-07 10:13:40 +0200

    Risolto problema inserimento numeri negativi

--- Calcolatrice-v2/Calcolatrice-v2.js
+++ Calcolatrice-v2/Calcolatrice-v2.js
@@ -1,4 +1,4 @@
-/* 
+﻿/* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -36,7 +36,7 @@ $(document).ready(function () {
         if (visore.length === 0 && this.innerHTML === "-") {
             visore += this.innerHTML;
             $("#campo").html(visore);
-        } else if (visore.length !== 0) {
+        } else if (visore.length !== 0 && visore.charAt(0)!="-") {
             str += visore;
             str += this.innerHTML;
             visore = "";
